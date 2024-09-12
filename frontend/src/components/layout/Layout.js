f52175e2d552cb44c742ac39cd3bd075893d7e08
Lakshya <lakshyavedik@gmail.com>
2024-09-13 00:25:09 +0530

    SEO

--- frontend/src/components/layout/Layout.js
+++ frontend/src/components/layout/Layout.js
@@ -1,15 +1,30 @@
 import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
+import { Helmet } from "react-helmet";
 
-const Layout = ({ children }) => {
+const Layout = ({ children, title, description, keywords, author }) => {
   return (
     <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+        <meta name="author" content={author} />
+        <title>{title}</title>
+      </Helmet>
       <Header />
       <main style={{ minHeight: "74vh" }}>{children}</main>
       <Footer />
     </div>
   );
+};
+
+Layout.defaultProps = {
+  title: "eStore | Your one stop shop",
+  description: "mern stack ecommerce app",
+  keywords: "mern, react, node, mongoDB",
+  authour: "rudeCactus",
 };
 
 export default Layout;
